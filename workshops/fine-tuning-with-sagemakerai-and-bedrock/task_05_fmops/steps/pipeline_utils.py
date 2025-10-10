c1b7f79a4a15634ccf55a1361362c50a94336e28
Dan Ferguson <frgud@amazon.com>
2025-10-10 16:18:35 -0400

    Fixed guardrail id bug

--- workshops/fine-tuning-with-sagemakerai-and-bedrock/task_05_fmops/steps/pipeline_utils.py
+++ workshops/fine-tuning-with-sagemakerai-and-bedrock/task_05_fmops/steps/pipeline_utils.py
@@ -62,39 +62,6 @@ def convert_to_messages(sample, system_prompt=""):
     
     return sample
 
-
-# Template dataset to add prompt to each sample
-# def template_dataset(sample):
-#     try:
-#         sample["text"] = PROMPT_TEMPLATE.format(question=sample["Question"],
-#                                                 complex_cot=sample["Complex_CoT"],
-#                                                 answer=sample["Response"])
-#         return sample
-#     except KeyError as e:
-#         print(f"KeyError in template_dataset: {str(e)}")
-#         # Provide default values for missing fields
-#         missing_key = str(e).strip("'")
-#         if missing_key == "Question":
-#             sample["text"] = PROMPT_TEMPLATE.format(
-#                 question="[Missing question]",
-#                 complex_cot=sample.get("Complex_CoT", "[Missing CoT]"),
-#                 answer=sample.get("Response", "[Missing response]")
-#             )
-#         elif missing_key == "Complex_CoT":
-#             sample["text"] = PROMPT_TEMPLATE.format(
-#                 question=sample["Question"],
-#                 complex_cot="[Missing CoT]",
-#                 answer=sample.get("Response", "[Missing response]")
-#             )
-#         elif missing_key == "Response":
-#             sample["text"] = PROMPT_TEMPLATE.format(
-#                 question=sample["Question"],
-#                 complex_cot=sample.get("Complex_CoT", "[Missing CoT]"),
-#                 answer="[Missing response]"
-#             )
-#         return sample
-
-
 def invoke_sagemaker_endpoint(payload, endpoint_name):
     """
     Invoke a SageMaker endpoint with the given payload.
@@ -123,65 +90,69 @@ def invoke_sagemaker_endpoint(payload, endpoint_name):
         return None, -1
 
 
+def create_guardrail(guardrail_client):
+    try:
+        guardrail = guardrail_client.create_guardrail(
+            name="ExampleMedicalGuardrail",
+            description='Example of a Guardrail for Medical Use Cases',
+            topicPolicyConfig={
+                'topicsConfig': [{
+                    'name': 'Block Pharmaceuticals',
+                    'definition': 'This model cannot recommend one pharmaceutical over another. Generic prescriptions consistent with medical expertise and clinical diagnoses only.',
+                    'type': 'DENY',
+                    'inputAction': 'BLOCK',
+                    'outputAction': 'BLOCK',
+                }]
+            },
+            sensitiveInformationPolicyConfig={
+                'piiEntitiesConfig': [
+                    {
+                        'type': 'UK_NATIONAL_HEALTH_SERVICE_NUMBER',
+                        'action': 'BLOCK',
+                        'inputAction': 'BLOCK',
+                        'outputAction': 'BLOCK'
+                    },
+                ]
+            },
+            contextualGroundingPolicyConfig={
+                'filtersConfig': [
+                    {
+                        'type': 'RELEVANCE',
+                        'threshold': 0.9,
+                        'action': 'BLOCK',
+                        'enabled': True
+                    },
+                ]
+            },
+            blockedInputMessaging="ExampleMedicalGuardrail has blocked this input.",
+            blockedOutputsMessaging="ExampleMedicalGuardrail has blocked this output."
+        )
+        guardrail_id = guardrail['guardrailId']
+        guardrail_version = guardrail['version']
+
+        print(f"Created new guardrail '{guardrail_id}:{guardrail_version}'")
+        return guardrail_id, guardrail_version
+    except botocore.exceptions.ClientError as create_error:
+        print(f"Error creating guardrail: {create_error}")
+
+
 def get_or_create_guardrail():
     guardrail_client = boto3.client('bedrock')
     guardrail_name = "ExampleMedicalGuardrail"
     try:
         # Try to get the guardrail
         response = guardrail_client.list_guardrails()
+        guardrail_id = ""
         for guardrail in response.get('guardrails', []):
             if guardrail['name'] == guardrail_name:
                 guardrail_id = guardrail['id']
-        response = guardrail_client.get_guardrail(
-            guardrailIdentifier=guardrail_id
-        )
-        guardrail_version = response["version"]
-        print(f"Found Guardrail {guardrail_id}:{guardrail_version}")
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # Guardrail doesn't exist, create it
-            try:
-                guardrail = guardrail_client.create_guardrail(
-                    name="ExampleMedicalGuardrail",
-                    description='Example of a Guardrail for Medical Use Cases',
-                    topicPolicyConfig={
-                        'topicsConfig': [{
-                            'name': 'Block Pharmaceuticals',
-                            'definition': 'This model cannot recommend one pharmaceutical over another. Generic prescriptions consistent with medical expertise and clinical diagnoses only.',
-                            'type': 'DENY',
-                            'inputAction': 'BLOCK',
-                            'outputAction': 'BLOCK',
-                        }]        
-                    },
-                    sensitiveInformationPolicyConfig={
-                        'piiEntitiesConfig': [
-                            {
-                                'type': 'UK_NATIONAL_HEALTH_SERVICE_NUMBER',
-                                'action': 'BLOCK',
-                                'inputAction': 'BLOCK',
-                                'outputAction': 'BLOCK'
-                            },
-                        ]
-                    },
-                    contextualGroundingPolicyConfig={
-                        'filtersConfig': [
-                            {
-                                'type': 'RELEVANCE',
-                                'threshold': 0.9,
-                                'action': 'BLOCK',
-                                'enabled': True
-                            },
-                        ]
-                    },
-                    blockedInputMessaging="ExampleMedicalGuardrail has blocked this input.",
-                    blockedOutputsMessaging="ExampleMedicalGuardrail has blocked this output."
-                )
-                guardrail_id = guardrail['guardrailId']
-                guardrail_version = guardrail['version']
-                
-                print(f"Created new guardrail '{guardrail_id}:{guardrail_version}'")
-            except botocore.exceptions.ClientError as create_error:
-                print(f"Error creating guardrail: {create_error}")
+        if guardrail_id != "":
+            response = guardrail_client.get_guardrail(
+                guardrailIdentifier=guardrail_id
+            )
+            guardrail_version = response["version"]
+            print(f"Found Guardrail {guardrail_id}:{guardrail_version}")
         else:
+            return create_guardrail(guardrail_client)
+    except botocore.exceptions.ClientError as e:
             print(f"Error checking guardrail: {e}")
-    return guardrail_id, guardrail_version
