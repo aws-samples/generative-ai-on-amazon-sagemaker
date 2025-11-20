7c0bace714c2c20bfc5f9ef7bd65a4c2d07faa67
Davide Gallitelli <dggallit@amazon.com>
2025-11-20 20:58:23 +0100

    Updated notebooks

--- workshops/efficient-model-for-agents-with-spectrum/utils/mlflow_tracking.py
+++ workshops/efficient-model-for-agents-with-spectrum/utils/mlflow_tracking.py
@@ -4,12 +4,10 @@ import boto3
 def get_mlflow_arn():
     """Lists all MLflow Tracking Servers in your AWS account using Boto3."""
     client = boto3.client('sagemaker')
-
     try:
         response = client.list_mlflow_tracking_servers()
         tracking_servers = response.get('TrackingServerSummaries', [])
         return tracking_servers[0]['TrackingServerArn']
-
     except Exception as e:
         print(f"Error listing MLflow Tracking Servers: {e}")
 
@@ -17,26 +15,14 @@ def get_mlflow_arn():
 def get_mlfow_url(tracking_server_name: str) -> str:
     # Initialize the SageMaker client
     sagemaker_client = boto3.client('sagemaker')
-
-    # Specify the name of your MLflow Tracking Server
-    # tracking_server_name = "aim410-mlflow-server" # Replace with your tracking server's name
-
     try:
         # Describe the MLflow tracking server
-        response = sagemaker_client.describe_mlflow_tracking_server(
+        response = sagemaker_client.create_presigned_mlflow_tracking_server_url(
             TrackingServerName=tracking_server_name
         )
-
         # Extract the TrackingServerUrl from the response
-        mlflow_ui_url = response.get('TrackingServerUrl')
-
-        if mlflow_ui_url:
-            print(
-                f"MLflow UI URL for '{tracking_server_name}': {mlflow_ui_url}"
-            )
-        else:
-            print(f"MLflow UI URL not found for '{tracking_server_name}'.")
-
+        mlflow_ui_url = response.get('AuthorizedUrl')
+        return f"[🔗️ Click here to open MLFlow 🧪]({mlflow_ui_url})"
     except sagemaker_client.exceptions.ResourceNotFoundException:
         print(f"MLflow Tracking Server '{tracking_server_name}' not found.")
     except Exception as e:
